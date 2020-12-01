c18eb5f66df56300dc476f7646a470c52821d598
wang zhen yu <asdfg10510588@gmail.com>
2020-12-01 19:33:01 +0800

    練習 7-3：在 AdminPostsController的 destroy 內刪除資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -27,6 +27,13 @@ class AdminPostsController extends Controller
         return redirect()->route('admin.posts.index');
     }
 
+    public function destroy($id)
+    {
+        Post::destroy($id);
+        return redirect()->route('admin.posts.index');
+    }
+
+
     public function create()
     {
         return view('admin.posts.create');
